5da6996fed734114b1b0454e06a9af6cd4ff9c05
salonigupta-sudo <saloni.gupta@students.asdrp.org>
2026-07-02 22:14:20 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
+import random
+
+print("Let's play hangman!")
 wordlist = [apple, horse, banana, blanket, table, piano, globe]
+picked_word = random.choice(wordlist)
+length = len(picked_word)
+display = "_ " * length
+print(display)
+lives = 6
